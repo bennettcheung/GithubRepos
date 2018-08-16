12826f15b4c921f4dadc650d1d77fba59493a751
Bennett <bennettcheung@yahoo.com>
2018-08-16 12:14:40 -0700

    Added table to load repo data into

---
+++ GithubRepos/Repo.h
@@ -0,0 +1,14 @@
+//
+//  Repo.h
+//  GithubRepos
+//
+//  Created by Bennett on 2018-08-16.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Repo : NSObject
+
+@property (nonatomic, strong) NSString *name;
+@end
---
+++ GithubRepos/Repo.m
@@ -0,0 +1,13 @@
+//
+//  Repo.m
+//  GithubRepos
+//
+//  Created by Bennett on 2018-08-16.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import "Repo.h"
+
+@implementation Repo
+
+@end
